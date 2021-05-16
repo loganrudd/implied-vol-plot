49ddef7b2275a5dd384afb7e7785d4b757f66122
Christopher Toledo <uberscientist@gmail.com>
2021-05-15 23:36:22 -0700

    fix README

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
-# implied-vol-plot
-Using the LedgerX public websocket API we're able to get live top of the orderbook updates for an option chain with over 150 different contracts and save the data to a RedisTimeSeries type as well as publish updates to listening subscribers.  An example subscriber, an interactive plot showing the implied volatility skew of the selected expiration and type (call or put).
+# Live BTC Implied Volatility Charts
+Using LedgerX public websocket API we're able to demonstrate using Redis as a multipurpose datastore.
+Using a server side Lua script to check the received updates counter we can appropriately publish PUBSUB messages to the
+listening [Bokeh](https://docs.bokeh.org/en/latest/) app and store the bid/ask prices to a 
+[RedisTimeSeries](https://oss.redislabs.com/redistimeseries/) data type atomically.
+
+The Bokeh app displays the implied volatility calculated from the best bid and offer prices received over websocket.
+We're using the Black-Scholes formula implemented by the [vollib](http://vollib.org/) library.
+
+We get the price of bitcoin from polling the coinbase API every 3 seconds.
+
+This allows traders to do further analysis and find opportunities in possible mispricings in the volatility component of
+the options pricing model.
 
 ## Setup
 
+Install [Redis](https://redis.io/) and [RedisTimeSeries](https://oss.redislabs.com/redistimeseries/)
+Next, clone the repository and install the dependencies in requirements.txt (`pip -r requirements.txt` in a venv)
+Now run `ledgerx_ws.py` which is the script consuming the websocket stream from LedgerX and run 
+`bokeh serve --show iv_app.py` from the project root to start up the Bokeh server application and open a web browser to
+the local URL.
+
+There are pre-cache files `contracts.pkl` and `id_table.json` which are loaded so no authenticated requests are needed.
+If you have a LedgerX account and API key, you can create a file named `secret` with the API key on the first line which
+will enable authenticated API queries.
+
+### Docker Install (WIP)
 Install docker: https://docs.docker.com/engine/install/
 
 Run the command `sysctl vm.overcommit_memory=1` to avoid background save failure under low memory condition
--- ledgerx_ws.py
+++ ledgerx_ws.py
@@ -1,22 +1,21 @@
 import websocket
 import json
 import redis
-# from redistimeseries.client import Client
 from ledgerx_api import get_contracts
 # redistimeseries docs: https://github.com/RedisTimeSeries/redistimeseries-py
 # websocket-client docs: https://github.com/websocket-client/websocket-client
 
 redis_host = 'localhost'
-# rts = Client(host=redis_host)
 r = redis.Redis(host=redis_host, decode_responses=True)
 
-# Lua script to do a conditional publish (only clock > prev_clock)
+# Lua script to do a conditional publish/store to TS (only clock > prev_clock)
 lua_cond_pub = """
 local prev_clock = redis.call('GETSET', KEYS[1], ARGV[1])
 local clock = tonumber(ARGV[1])
 prev_clock = tonumber(prev_clock)
 if(clock > prev_clock) then
-    redis.call('PUBLISH', ARGV[2], ARGV[3])
+    redis.call('PUBLISH', ARGV[2], ARGV[5])
+    redis.call('TS.MADD', KEYS[2], ARGV[3], KEYS[3], ARGV[4])
     return 1
 else
     return 0
@@ -30,10 +29,14 @@ def on_message(ws, raw_message):
     try:
         ws_contract_id = message['contract_id']
         clock = message['clock']
+        bid = message['bid']
+        ask = message['ask']
 
         # Publish websocket message to PUBSUB channel
         channel = f"{ws_contract_id}.1"
-        cond_pub(keys=[f'{ws_contract_id}:clock'], args=[clock, channel, raw_message])
+        lua_keys = [f'{ws_contract_id}:clock', f'{ws_contract_id}:bid', f'{ws_contract_id}:ask']
+        lua_args = [clock, channel, bid, ask, raw_message]
+        cond_pub(keys=lua_keys, args=lua_args)
     except KeyError:
         print('Unknown data format:')
         print(message)
@@ -53,20 +56,8 @@ def on_open(ws):
 
 
 if __name__ == "__main__":
-    # websocket.enableTrace(True)
-    # Initialize database state:
-    # get currently active contracts
-    contracts = get_contracts(active=True)
-    option_chain = contracts['option_chain']
-
-    # Loop over options
-    active_options = []
-    for expiry in option_chain:
-        for contract in option_chain[expiry]:
-            contract_id = contract['id']
-            active_options.append(contract_id)
-
     url = f"wss://api.ledgerx.com/ws"
+    r.flushdb()
     ledgerx_ws = websocket.WebSocketApp(url,
                                         on_open=on_open,
                                         on_message=on_message,
